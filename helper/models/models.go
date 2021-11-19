761d3df588eacf4372da97d91e9f3194fe535067
Anggun Permata <anggunpermata.g789@gmail.com>
2021-11-19 16:21:24 +0700

    repair models.go

--- helper/models/models.go
+++ helper/models/models.go
@@ -2,5 +2,5 @@ package models
 
 type UrlShortener_Payload struct{
 	OriginalURL string `json:"original_url" form:"original_url"`
-	ShortenedURL string `json:"shortened_url" form:"original_url"`
+	ShortenedURL string `json:"shortened_url" form:"shortened_url"`
 }
